7716a30a440fe94b6f807f9afced9e25d9455e73
Mark Grebenshchikov <marc.grebenshchikov@yandex.ru>
2024-11-23 03:06:15 -0500

    First Commit

---
+++ src/test/java/FelineTest.java
@@ -0,0 +1,58 @@
+import com.example.Feline;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class FelineTest {
+    @Test
+    public void eatMeatReturnPredatorListOfFoodTest() throws Exception {
+        Feline feline = new Feline();
+        List<String> expectedResult = List.of("Животные", "Птицы", "Рыба");
+
+        List<String> actualResult = feline.eatMeat();
+        System.out.println(String.format("actualResult = %s", actualResult));
+
+        assertEquals(expectedResult,actualResult);
+
+    }
+
+    @Test
+    public void getFamilyReturnFelineTest() {
+        Feline feline = new Feline();
+        String expectedResult = "Кошачьи";
+
+        String actualResult= feline.getFamily();
+        System.out.println(String.format("actualResult = %s", actualResult));
+
+        assertEquals(expectedResult,actualResult);
+
+    }
+
+    @Test
+    public void getKittensReturnOneKittenTest() {
+        Feline feline = new Feline();
+        int expectedResult = 1;
+
+        int actualResult= feline.getKittens();
+        System.out.println(String.format("actualResult = %s", actualResult));
+
+        assertEquals(expectedResult,actualResult);
+
+    }
+
+    @Test
+    public void getKittensReturnFiveTest() {
+        Feline feline = new Feline();
+        int expectedResult = 5;
+
+        int actualResult= feline.getKittens(5);
+        System.out.println(String.format("actualResult = %s", actualResult));
+
+        assertEquals(expectedResult,actualResult);
+
+    }
+
+
+}
---
+++ src/test/java/LionTests/ParameterizedLionTests.java
@@ -0,0 +1,84 @@
+package LionTests;
+
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+@RunWith(Parameterized.class)
+public class ParameterizedLionTests {
+
+    private final String lionSex;
+    private final boolean hasMane;
+    private final String info;
+
+    public ParameterizedLionTests(String lionSex, boolean hasMane, String info) {
+        this.lionSex = lionSex;
+        this.hasMane = hasMane;
+        this.info = info;
+    }
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Parameterized.Parameters()
+    public static Object[][] getDate(){
+        return new Object[][]{
+                {"Самец", true, "У самца есть грива"},
+                {"Самка", false, "У самки нет гривы"}
+        };
+    }
+    @Mock
+    Feline felineMock;
+
+    @Test
+    public void getKittensReturnOneKittenTest() throws Exception {
+
+        Lion lion = new Lion(lionSex,felineMock);
+        int expectedResult = 1;
+
+        Mockito.when(felineMock.getKittens()).thenReturn(1);
+        int actualResult = lion.getKittens();
+        System.out.println(String.format("actualResult = %s", actualResult));
+
+        assertEquals(expectedResult,actualResult);
+    }
+
+    @Test
+    public void doesHaveManeTest() throws Exception {
+
+        Lion lion = new Lion(lionSex,felineMock);
+        boolean expectedResult = hasMane;
+
+        boolean actualResult = lion.doesHaveMane();
+        System.out.println(info + String.format(" (actualResult = %s", actualResult + ")"));
+
+        assertEquals(expectedResult,actualResult);
+    }
+
+    @Test
+    public void getFoodReturnCarnivoreTest() throws Exception {
+
+        Lion lion = new Lion(lionSex,felineMock);
+        List <String> expectedResult = List.of("Животные", "Птицы", "Рыба");
+
+        Mockito.when(felineMock.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        List <String> actualResult = lion.getFood();
+
+        System.out.println(String.format("actualResult = %s", actualResult));
+        assertEquals(expectedResult,actualResult);
+    }
+}
+
